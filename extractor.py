8c393d7bef28b0256850a8d4fb28423f24b880d1
Levi Lentz <llentz@SWIZZLE.MIT.EDU>
2017-10-27 13:15:29 -0400

    Module fix to extractor.py

--- extractor.py
+++ extractor.py
@@ -1,62 +1,75 @@
 #!/usr/bin/env python
-
-import tempfile
 import sys
 import glob
 import QE
 from bandgapoccu import bg as BG
 import os
 import json
-import io
 import re
-temp = [zz.split()[2] for zz in open(sys.argv[1]).read().split('\n') if 'temperature' in zz]
-temp = [float(zz) for zz in temp if re.match('\d+.\d+$',zz)]
-scf = open(sys.argv[1],'r')
 
-header = ''
+def separate_aimd(fname='scf.out',tempfile='OUT.out',jfile='data.json'):
+  """This takes a AIMD run from QE and separates the data into jfile
+run as separate_aimd(fname=QE out file, tempfile = Temporary file used for parsing, jfile = json file for output
+  """
+  temp = [zz.split()[2] for zz in open(fname).read().split('\n') if 'temperature' in zz]
+  temp = [float(zz) for zz in temp if re.match('\d+.\d+$',zz)]
+  scf = open(fname,'r')
 
-for i in scf:
-  if 'PseudoPot' in i: break
-  header += i
+  header = ''
 
-
-data = {}
-rest = header + '\n\n'
-tmp = ''
-tmp += rest
-cnt = 1
-try:
   for i in scf:
-    while '!    total energy' not in i:
+    if 'PseudoPot' in i: break
+    header += i
+
+
+  data = []
+  rest = header + '\n\n'
+  tmp = ''
+  tmp += rest
+  cnt = 1
+  base = QE.Struct()
+  try:
+    for i in scf:
+      while '!    total energy' not in i:
+        i = next(scf)
+        tmp += i
+      #data[cnt] = {}
+      t_ = {}
+      OUT = tempfile
+      output = open(OUT,'w')
+      output.write(tmp)
+      output.close()
+      x = QE.Struct()
+      x.File_Process(OUT)
+      atoms,cell = x.return_params()
+      if cnt == 1: base.File_Process(OUT)
+      displ = []
+      for zz in base.atoms:
+        displ.append(list(base.atoms[zz]-x.atoms[zz]))
+      t_['str'] = x.print()
+      t_['atoms'] = atoms
+      t_['cell'] = cell
+      t_['displ'] = displ 
+      del x
+      Gap = BG(OUT)
+      t_['gap'] = Gap.bg
+      try:
+        t_['temp'] = temp[cnt - 1] #[zz for zz in open(str(cnt) + '.out').read().split('\n') if 'temperature' in zz][-1].split()[2]
+      except:
+        t_['temp'] = 'NA'
+      cnt += 1
+      tmp = rest
+      data.append(t_)
+      with open(jfile, 'w') as outfile:
+          json.dump(data, outfile)
       i = next(scf)
-      tmp += i
-    #if not os.path.exists('%s.save'%cnt): os.makedirs('%s.save'%cnt)
-    data[cnt] = {}
-    OUT = 'OUT.out'
-    output = open(OUT,'w')
-    output.write(tmp)
-    output.close()
-    x = QE.Struct()
-    x.File_Process(OUT)
-    atoms,cell = x.return_parms()
-    data[cnt]['str'] = x.print()
-    data[cnt]['atoms'] = atoms
-    data[cnt]['cell'] = cell
-    del x
-    Gap = BG(OUT)
-    data[cnt]['gap'] = Gap.bg
-    try:
-      data[cnt]['temp'] = temp[cnt - 1] #[zz for zz in open(str(cnt) + '.out').read().split('\n') if 'temperature' in zz][-1].split()[2]
-    except:
-      data[cnt]['temp'] = 'NA'
-    cnt += 1
-    tmp = rest
-    with open('data.json', 'w') as outfile:
+      continue
+    scf.close()
+  except StopIteration:
+    scf.close()
+    with open(jfile, 'w') as outfile:
         json.dump(data, outfile)
-    i = next(scf)
-    continue
-  scf.close()
-except:
-  scf.close()
-  with open('data.json', 'w') as outfile:
-      json.dump(data, outfile)
+
+if __name__ == '__main__':
+  separate_aimd(fname=sys.argv[1])
+
